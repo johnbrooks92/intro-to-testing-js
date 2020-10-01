9c9f581ca5fafe004518464e067fdf7a130e8708
John <johntbrooks92@gmail.com>
2020-10-01 16:50:29 -0500

    Exercise #12 Complete

--- code.js
+++ code.js
@@ -30,3 +30,11 @@ function isEven(x) {
     else
         return true;
 }
+function isVowel(x)
+{
+    if (x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u' || x == 'A' || x == 'E' || x == 'I' || x == 'O' || x == 'U')
+        return true;
+    else
+        return false;
+}
+
--- tests.js
+++ tests.js
@@ -82,3 +82,35 @@ describe('isEven', function() {
         expect(isEven()).toBe(false);
     });
 });
+
+describe('isVowel', function() {
+    it('should be a defined function', function() {
+        expect(typeof isVowel).toBe('function');
+    });
+    it('should return a boolean no matter the input', function() {
+        expect(typeof isVowel()).toBe('boolean');
+    });
+    it('should return "true" when passed a vowel', function() {
+        expect(isVowel("a")).toBe(true);
+    });
+    it('should return "true" when passed a vowel', function() {
+        expect(isVowel("A")).toBe(true);
+    });
+    it('should return "false" when passed a consonant', function() {
+        expect(isVowel("y")).toBe(false);
+    });
+    it('should return "false" when passed a number', function() {
+        expect(isVowel(4)).toBe(false);
+    });
+    it('should return "false" when passed a boolean', function() {
+        expect(isVowel(true)).toBe(false);
+        expect(isVowel(false)).toBe(false);
+    });
+    it('should return "false" when passed a string', function() {
+        expect(isVowel("banana")).toBe(false);
+    });
+    it('should return "false" when called without an argument', function() {
+        expect(isVowel()).toBe(false);
+    });
+});
+
